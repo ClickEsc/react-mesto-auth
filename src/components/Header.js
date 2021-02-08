0bc543084107a173c5462915759221c85378cb0a
Irina Skubilina-Burtseva <zebra-head@yandex.ru>
2021-02-09 00:08:59 +0300

    Feat: Update Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,10 @@ function Header() {
   return (
     <header className="header">
       <div className="header__logo"></div>
+      <div className="header__info">
+        <p className="header__user">email@mail.com</p>
+        <a className="header__link">Выйти</a>
+      </div>
     </header>
   );
 }
